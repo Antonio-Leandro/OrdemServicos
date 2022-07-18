df124eedc28349964687c363e7f3a1a542ac2711
Antonio-Leandro <61568166+Antonio-Leandro@users.noreply.github.com>
2022-07-18 15:19:38 -0300

    Alteração no layout do formulário de Nimbu para Windows.

--- src/br/com/servicos/forms/TelaLogin.java
+++ src/br/com/servicos/forms/TelaLogin.java
@@ -149,7 +149,7 @@ public void logar(){
          */
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
+                if ("Windows".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
                 }
